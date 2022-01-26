708e411eec07ca9b4bfc68c50edcf6094ebb1a69
xovest <79008917+xovest@users.noreply.github.com>
2022-01-26 12:57:07 +0200

    migrations

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,14 +1,27 @@
 pragma solidity ^0.5.0;
 
+// modelling -> storing -> uploading -> listing
+
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  mapping(uint => Video) public videos; //storing and listing
 
-  //Create Struct
+  //(here we model)
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address author;
+  }
 
 
-  //Create Event
+  event VideoUploaded(
+    uint id,
+    string hash,
+    string title,
+    address author
+  );
 
 
   constructor() public {
@@ -16,17 +29,22 @@ contract DVideo {
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
     // Make sure the video hash exists
+    require(bytes(_videoHash).length > 0);
 
     // Make sure video title exists
+    require(bytes(_title).length > 0);
 
     // Make sure uploader address exists
-
+    require(msg.sender != address(0));
 
     // Increment video id
+    ++videoCount;
 
-    // Add video to the contract
+    // Add video to the contract and storing on the blockchain
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
 
     // Trigger an event
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
 
   }
 }
